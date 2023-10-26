d5d6c053899c8f12d6166aa798d94f0b6f27a855
Andreas Blech <blech@permarobotics.com>
2023-10-26 15:46:04 +0200

    replaced checkbox with toggle

--- main.py
+++ main.py
@@ -15,34 +15,34 @@ def main():
     antworten = {}
 
     # Fragen mit Ja/Nein Optionen
-    antworten[1] = st.checkbox(
+    antworten[1] = st.toggle(
         "1. Befindet sich Ihr Dauergrünland in einem umweltsensiblen Bereich oder in Feucht- und Moorgebieten?"
     )
-    antworten[2] = st.checkbox(
+    antworten[2] = st.toggle(
         "2. Haben Sie bereits eine Genehmigung zur Umwandlung beantragt oder wurden Ihnen andere Rechtsvorschriften oder Verpflichtungen auferlegt, die einer Umwandlung entgegenstehen?"
     )
-    antworten[3] = st.checkbox(
+    antworten[3] = st.toggle(
         "3. Ist der Dauergrünlandanteil in Ihrer Region in den letzten Jahren um mehr als 4 Prozent gesunken?"
     )
-    antworten[4] = st.checkbox(
+    antworten[4] = st.toggle(
         "4. Handelt es sich bei Ihrem Dauergrünland um einen Grünlandlebensraumtyp des Anhangs I der FFH-Richtlinie?"
     )
-    antworten[5] = st.checkbox(
+    antworten[5] = st.toggle(
         "5. Haben Sie die Bereitschaft, an anderer Stelle in derselben Region eine Ersatzfläche für Dauergrünland anzulegen?"
     )
-    antworten[6] = st.checkbox(
+    antworten[6] = st.toggle(
         "6. Falls die Fläche nicht in Ihrem Besitz ist, haben Sie die Zustimmung des Eigentümers zur Neuanlage dieser Fläche als Dauergrünland?"
     )
-    antworten[7] = st.checkbox(
+    antworten[7] = st.toggle(
         "7. Können Sie nachweisen, dass die Fläche, die Sie als Dauergrünland neu anlegen wollen, mindestens fünf Jahre für den Anbau genutzt wird?"
     )
-    antworten[8] = st.checkbox(
+    antworten[8] = st.toggle(
         "8. Handelt es sich um Flächen, die im Rahmen von GAP-Maßnahmen entstanden sind oder erst ab 2015 neu entstanden sind?"
     )
-    antworten[9] = st.checkbox(
+    antworten[9] = st.toggle(
         "9. Haben Dauergrünlandflächen, die neu angelegt wurden, mindestens fünf Jahre für den Anbau gedient?"
     )
-    antworten[10] = st.checkbox(
+    antworten[10] = st.toggle(
         "10. Haben Sie Dauergrünland ohne Genehmigung umgewandelt, das ab dem 1. Januar 2021 neu entstanden ist?"
     )
 
