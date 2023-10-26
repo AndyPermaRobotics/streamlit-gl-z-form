a01d378fe03e98a70a6692563c3fe3cf1187afbf
Andreas Blech <blech@permarobotics.com>
2023-10-26 15:57:10 +0200

    radio buttons

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import streamlit as st
 
+from src.gpt import get_completion
+
 
 # Funktion, um die Benutzerantworten zu speichern
 def save_responses(antworten):
-    # Hier könnten Sie die Antworten speichern oder weiterverarbeiten
-    # Zum Beispiel in einer Datei speichern oder in einer Datenbank
-    pass
+    prompt = """
+
+
+    """
+
+    answer = get_completion(prompt)
+
+    # show answer in the streamlit app
 
 
 def main():
@@ -14,41 +21,78 @@ def main():
     # Initialisieren eines leeren Wörterbuchs für die Antworten
     antworten = {}
 
-    # Fragen mit Ja/Nein Optionen
-    antworten[1] = st.toggle(
-        "1. Befindet sich Ihr Dauergrünland in einem umweltsensiblen Bereich oder in Feucht- und Moorgebieten?"
-    )
-    antworten[2] = st.toggle(
-        "2. Haben Sie bereits eine Genehmigung zur Umwandlung beantragt oder wurden Ihnen andere Rechtsvorschriften oder Verpflichtungen auferlegt, die einer Umwandlung entgegenstehen?"
-    )
-    antworten[3] = st.toggle(
-        "3. Ist der Dauergrünlandanteil in Ihrer Region in den letzten Jahren um mehr als 4 Prozent gesunken?"
-    )
-    antworten[4] = st.toggle(
-        "4. Handelt es sich bei Ihrem Dauergrünland um einen Grünlandlebensraumtyp des Anhangs I der FFH-Richtlinie?"
-    )
-    antworten[5] = st.toggle(
-        "5. Haben Sie die Bereitschaft, an anderer Stelle in derselben Region eine Ersatzfläche für Dauergrünland anzulegen?"
-    )
-    antworten[6] = st.toggle(
-        "6. Falls die Fläche nicht in Ihrem Besitz ist, haben Sie die Zustimmung des Eigentümers zur Neuanlage dieser Fläche als Dauergrünland?"
-    )
-    antworten[7] = st.toggle(
-        "7. Können Sie nachweisen, dass die Fläche, die Sie als Dauergrünland neu anlegen wollen, mindestens fünf Jahre für den Anbau genutzt wird?"
-    )
-    antworten[8] = st.toggle(
-        "8. Handelt es sich um Flächen, die im Rahmen von GAP-Maßnahmen entstanden sind oder erst ab 2015 neu entstanden sind?"
-    )
-    antworten[9] = st.toggle(
-        "9. Haben Dauergrünlandflächen, die neu angelegt wurden, mindestens fünf Jahre für den Anbau gedient?"
-    )
-    antworten[10] = st.toggle(
-        "10. Haben Sie Dauergrünland ohne Genehmigung umgewandelt, das ab dem 1. Januar 2021 neu entstanden ist?"
-    )
+    # Definition der Fragen
+    fragen = {
+        1: "Befindet sich Ihr Dauergrünland in einem umweltsensiblen Bereich oder in Feucht- und Moorgebieten?",
+        2: "Haben Sie bereits eine Genehmigung zur Umwandlung beantragt oder wurden Ihnen andere Rechtsvorschriften oder Verpflichtungen auferlegt, die einer Umwandlung entgegenstehen?",
+        3: "Ist der Dauergrünlandanteil in Ihrer Region in den letzten Jahren um mehr als 4 Prozent gesunken?",
+        4: "Handelt es sich bei Ihrem Dauergrünland um einen Grünlandlebensraumtyp des Anhangs I der FFH-Richtlinie?",
+        5: "Haben Sie die Bereitschaft, an anderer Stelle in derselben Region eine Ersatzfläche für Dauergrünland anzulegen?",
+        6: "Falls die Fläche nicht in Ihrem Besitz ist, haben Sie die Zustimmung des Eigentümers zur Neuanlage dieser Fläche als Dauergrünland?",
+        7: "Können Sie nachweisen, dass die Fläche, die Sie als Dauergrünland neu anlegen wollen, mindestens fünf Jahre für den Anbau genutzt wird?",
+        8: "Handelt es sich um Flächen, die im Rahmen von GAP-Maßnahmen entstanden sind oder erst ab 2015 neu entstanden sind?",
+        9: "Haben Dauergrünlandflächen, die neu angelegt wurden, mindestens fünf Jahre für den Anbau gedient?",
+        10: "Haben Sie Dauergrünland ohne Genehmigung umgewandelt, das ab dem 1. Januar 2021 neu entstanden ist?",
+    }
+
+    # Erstellen der Ja/Nein Toggles für jede Frage
+    # for nummer, frage in fragen.items():
+    #     antworten[nummer] = st.radio(f"Frage {nummer}: {frage}", ('Ja', 'Nein'))
+
+    # # Fragen mit Ja/Nein Optionen
+    # antworten[1] = st.toggle(
+    #     "1. Befindet sich Ihr Dauergrünland in einem umweltsensiblen Bereich oder in Feucht- und Moorgebieten?"
+    # )
+    # antworten[2] = st.toggle(
+    #     "2. Haben Sie bereits eine Genehmigung zur Umwandlung beantragt oder wurden Ihnen andere Rechtsvorschriften oder Verpflichtungen auferlegt, die einer Umwandlung entgegenstehen?"
+    # )
+    # antworten[3] = st.toggle(
+    #     "3. Ist der Dauergrünlandanteil in Ihrer Region in den letzten Jahren um mehr als 4 Prozent gesunken?"
+    # )
+    # antworten[4] = st.toggle(
+    #     "4. Handelt es sich bei Ihrem Dauergrünland um einen Grünlandlebensraumtyp des Anhangs I der FFH-Richtlinie?"
+    # )
+    # antworten[5] = st.toggle(
+    #     "5. Haben Sie die Bereitschaft, an anderer Stelle in derselben Region eine Ersatzfläche für Dauergrünland anzulegen?"
+    # )
+    # antworten[6] = st.toggle(
+    #     "6. Falls die Fläche nicht in Ihrem Besitz ist, haben Sie die Zustimmung des Eigentümers zur Neuanlage dieser Fläche als Dauergrünland?"
+    # )
+    # antworten[7] = st.toggle(
+    #     "7. Können Sie nachweisen, dass die Fläche, die Sie als Dauergrünland neu anlegen wollen, mindestens fünf Jahre für den Anbau genutzt wird?"
+    # )
+    # antworten[8] = st.toggle(
+    #     "8. Handelt es sich um Flächen, die im Rahmen von GAP-Maßnahmen entstanden sind oder erst ab 2015 neu entstanden sind?"
+    # )
+    # antworten[9] = st.toggle(
+    #     "9. Haben Dauergrünlandflächen, die neu angelegt wurden, mindestens fünf Jahre für den Anbau gedient?"
+    # )
+    # antworten[10] = st.toggle(
+    #     "10. Haben Sie Dauergrünland ohne Genehmigung umgewandelt, das ab dem 1. Januar 2021 neu entstanden ist?"
+    # )
+
+    # Variable zur Überprüfung, ob alle Fragen beantwortet wurden
+    alle_beantwortet = True
+
+    # Erstellen der Ja/Nein Toggles für jede Frage
+    for nummer, frage in fragen.items():
+        antwort = st.radio(
+            f"Frage {nummer}: {frage}", ["Ja", "Nein", "Unbeantwortet"], index=2
+        )
+
+        # Überprüfen, ob die Frage unbeantwortet ist
+        if antwort == "Unbeantwortet":
+            st.warning(f"Frage {nummer} ist unbeantwortet.")
+            alle_beantwortet = False
+
+        antworten[nummer] = antwort
 
     if st.button("Antworten speichern"):
-        save_responses(antworten)
-        st.success("Ihre Antworten wurden gespeichert.")
+        if alle_beantwortet:
+            save_responses(antworten)
+            st.success("Ihre Antworten wurden gespeichert.")
+        else:
+            st.error("Bitte beantworten Sie alle Fragen, bevor Sie fortfahren.")
 
 
 if __name__ == "__main__":
---
+++ src/gpt.py
@@ -0,0 +1,144 @@
+import json
+import os
+import time
+from typing import Dict
+
+import openai
+
+openai.api_key  = os.getenv('OPENAI_API_KEY')
+
+def get_current_date():
+    """Get the current date"""
+    
+    obj = {
+        "current_date": time.strftime("%Y-%m-%d")
+    }
+    
+    return json.dumps(obj)
+
+
+def get_completion(prompt, model="gpt-3.5-turbo-16k", temperature=0) -> str: 
+    messages = [{"role": "user", "content": prompt}]
+    
+    for i in range(3):
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=messages,
+                temperature=temperature, 
+
+                #Step 1 - provide functions metadata
+                functions=[
+                    # {
+                    #     "name": "get_current_weather",
+                    #     "description": "Kann Auskunft über das aktuelle Wetter geben ",
+                    #     "parameters": {
+                    #         "type": "object",
+                    #         "properties": {
+                    #             "location": {
+                    #                 "type": "string",
+                    #                 "description": "Die Stadt, für die das Wetter abgefragt werden soll",
+                    #             },
+                    #             "unit": {
+                    #                 "type": "string", 
+                    #                 "enum": ["celsius", "fahrenheit"]
+                    #             },
+                    #         },
+                    #         "required": ["location"],
+                    #     },
+                    # },
+                    {
+                        "name": "get_current_date",
+                        "description": "Kann das aktuelle Datum zurückgeben",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {}, # no parameters needed
+                        }, 
+                    },
+                    # {
+                    #     "name": "execute_python",
+                    #     "description": "Kann Python Code ausführen",
+                    #     "parameters": {
+                    #         "type": "object",
+                    #         "properties": {
+                    #             "code": {
+                    #                 "type": "string",
+                    #                 "description": "Der Python Code, der ausgeführt werden soll",
+                    #             },
+                    #         },
+                    #         "required": ["code"],
+                    #     }, 
+                    # }
+                ],
+                function_call="auto",
+            )
+
+            if isinstance(response, Dict):
+                message = response["choices"][0]["message"]
+
+                print(f"Received message: {message}")
+
+                # Step 2, check if the model wants to call a function
+                if message.get("function_call"):
+                    function_name = message["function_call"]["name"]
+
+                    print(f"Calling function: {function_name}")
+                    print(f"Function parameters: {json.dumps(message)}")
+
+                    arguments = json.loads(message["function_call"]["arguments"])    
+
+                    return_message = ""
+
+
+                    print(f"{arguments=}")
+
+                    if function_name == "get_current_date":
+                        function_response = get_current_date()
+                    elif function_name == "execute_python":
+                        code = arguments.get("code")
+
+                        print(f"{code=}")
+
+                        return_message = f"Python Code:\n```{code}```"
+
+                        if isinstance(code, str):
+                            function_response = eval(code)
+                        else:
+                            raise Exception(f"Code is not a string: {code}")
+                    
+                    else:
+                        raise Exception(f"Unknown function: {function_name}")
+
+                    # Step 4, send model the info on the function call and function response
+                    second_response = openai.ChatCompletion.create(
+                        model=model,
+                        messages=[
+                            {"role": "user", "content": prompt},
+                            message,
+                            {
+                                "role": "function",
+                                "name": function_name,
+                                "content": str(function_response),
+                            },
+                        ],
+                    )
+
+                    if isinstance(second_response, Dict):
+                        content = second_response["choices"][0]["message"]["content"]
+                        
+                        if(len(return_message) > 0):
+                            return return_message + "\n\n" +  content
+                        else:
+                            return content
+
+                else:
+                    #if no function call is needed, return the message content
+                    return message["content"]
+            
+        except openai.APIError:
+            print(f"API Error occurred, retrying ({i+1}/3)...")
+            time.sleep(1)
+        except Exception as e:
+            print(f"Error occurred: {e}. Retrying ({i+1}/3)...")
+            time.sleep(3)
+    raise Exception("Failed to get response from OpenAI after 3 retries")
